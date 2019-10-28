098bcd24d20e6869151bd1c8405dffea94f82e40
Pedro Carrega <pedro.carrega@outlook.com>
2019-10-28 19:56:58 +0000

    adding some benchmarks

--- Assignments/Assignment_01/Assignment_01_code/src/knapsack/KnapsackGA.java
+++ Assignments/Assignment_01/Assignment_01_code/src/knapsack/KnapsackGA.java
@@ -7,10 +7,10 @@ import java.util.stream.IntStream;
 
 public class KnapsackGA {
 	private static final int N_GENERATIONS = 500;
-	private static final int POP_SIZE = 100000;
+	private static final int POP_SIZE = 500000;
 	private static final double PROB_MUTATION = 0.5;
 	private Individual[] population = new Individual[POP_SIZE];
-	
+
 	//private Random r = new Random();
 
 	public KnapsackGA() {
@@ -18,27 +18,27 @@ public class KnapsackGA {
 	}
 
 	private void populateInitialPopulationRandomly() {
-		
+
 		/*for (int i=0; i<POP_SIZE; i++) {
 			population[i] = Individual.createRandom();
 		}*/
-		
+
 		IntStream.range(0, POP_SIZE)
-		.parallel()
-		.forEach((int i) -> {
-			population[i] = Individual.createRandom(); 
-		});
+				 .parallel()
+				 .forEach((int i) -> {
+					 	population[i] = Individual.createRandom(); 
+				 });
 	}
 
 	public void run() {
 		for (int generation=0; generation<N_GENERATIONS; generation++) {
 
 			IntStream.range(0, POP_SIZE)
-			.parallel()
-			.forEach((int i) -> {
-				population[i].measureFitness(); 
-			});
-			
+					 .parallel()
+					 .forEach((int i) -> {
+						 	population[i].measureFitness(); 
+					 });
+
 			/*
 			// Step1 - Calculate Fitness
 			for (int i=0; i<POP_SIZE; i++) {
@@ -58,17 +58,17 @@ public class KnapsackGA {
 			newPopulation[0] = population[0]; // The best individual remains
 
 			IntStream.range(1, POP_SIZE)
-			.parallel()
-			.forEach((int i) -> {
-				
-				ThreadLocalRandom r = ThreadLocalRandom.current();
-				
-				int pos1 = (int) (- Math.log(r.nextDouble()) * POP_SIZE) % POP_SIZE;
-				int pos2 = (int) (- Math.log(r.nextDouble()) * POP_SIZE) % POP_SIZE;
+					 .parallel()
+					 .forEach((int i) -> {
 
-				newPopulation[i] = population[pos1].crossoverWith(population[pos2]); 
-			});
-			
+						 	ThreadLocalRandom r = ThreadLocalRandom.current();
+
+						 	int pos1 = (int) (- Math.log(r.nextDouble()) * POP_SIZE) % POP_SIZE;
+						 	int pos2 = (int) (- Math.log(r.nextDouble()) * POP_SIZE) % POP_SIZE;
+
+						 	newPopulation[i] = population[pos1].crossoverWith(population[pos2]); 
+					 });
+
 			/*
 			for (int i=1; i<POP_SIZE; i++) {
 				// The first elements in the population have higher probability of being selected
@@ -77,17 +77,17 @@ public class KnapsackGA {
 
 				newPopulation[i] = population[pos1].crossoverWith(population[pos2]);
 			}
-			*/
-			
+			 */
+
 			IntStream.range(1, POP_SIZE)
-			 .parallel()
-			 .forEach((int i) -> {
-				 
-				 ThreadLocalRandom r = ThreadLocalRandom.current();
-				 if (r.nextDouble() < PROB_MUTATION) {
-						newPopulation[i].mutate();
-					}
-			 });
+					 .parallel()
+					 .forEach((int i) -> {
+
+						 	ThreadLocalRandom r = ThreadLocalRandom.current();
+						 	if (r.nextDouble() < PROB_MUTATION) {
+						 		newPopulation[i].mutate();
+						 	}
+					 });
 
 			/*
 			// Step4 - Mutate
@@ -96,7 +96,7 @@ public class KnapsackGA {
 					newPopulation[i].mutate();
 				}
 			}
-			*/
+			 */
 			population = newPopulation;
 		}
 	}
