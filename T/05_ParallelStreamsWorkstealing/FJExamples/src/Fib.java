113bbc795aa4a6d69e2c9f4584f3e6a65cc56a94
Pedro Carrega <pedro.carrega@outlook.com>
2019-11-06 01:22:22 +0000

    remove warning

--- T/05_ParallelStreamsWorkstealing/FJExamples/src/Fib.java
+++ T/05_ParallelStreamsWorkstealing/FJExamples/src/Fib.java
@@ -2,6 +2,7 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
 
+@SuppressWarnings("serial")
 public class Fib extends RecursiveTask<Integer> {
 	public static void main(String[] args) {
 		
