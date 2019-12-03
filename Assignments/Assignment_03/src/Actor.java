063bed7dd7d446480574fd9118d6fc9a11d0288d
Pedro Carrega <fc49480@alunos.fc.ul.pt>
2019-12-03 10:34:14 +0000

    implementing node

--- Assignments/Assignment_03/src/Actor.java
+++ Assignments/Assignment_03/src/Actor.java
@@ -2,4 +2,34 @@
 
 public class Actor {
 
+	private Actor left;
+	private Actor right;
+	private int value;
+
+	public Actor(Message m) {
+		left = null;
+		right = null;
+		value = m.getNumber();
+	}
+
+	public void handleMessage(Message m) {
+		if(left == null && value > m.getNumber())
+			left = new Actor(m);
+		else if(right == null && value < m.getNumber())
+			right = new Actor(m);
+		else
+			foward(m);
+		
+	}
+
+	public void foward(Message m) {
+		if(left.getValue() > m.getNumber())
+			left.handleMessage(m);
+		else
+			right.handleMessage(m);
+	}
+
+	public int getValue() {
+		return this.value;
+	}
 }
