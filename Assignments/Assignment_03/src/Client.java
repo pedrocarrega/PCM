6642ab31debbecd24de2b1f4b7525ea1adadf952
Pedro Carrega <pedro.carrega@outlook.com>
2019-12-05 13:21:08 +0000

    bug fix

--- Assignments/Assignment_03/src/Client.java
+++ Assignments/Assignment_03/src/Client.java
@@ -3,7 +3,7 @@ public class Client {
 	public static void main(String[] args) {
 		
 		RootActor root = new RootActor();
-		root.recieveMessage(new StartMessage());
+		root.receiveMessage(new StartMessage());
 		root.start();
 		
 	}
